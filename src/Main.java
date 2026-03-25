f1c6c197292d2935ec5ec304d27b306da8c54e3a
assignment1 <riftlik@gmail.com>
2026-03-25 23:47:57 +0500

    tss

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,49 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+import static java.lang.Math.pow;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] arr_num = {1,2,3,4,5};
+        System.out.println(sum(4));
+        System.out.println(sum_arr(arr_num , 4));
+        System.out.println(power(4 , 3));
+        System.out.println(revers(3 , scanner));
+    }
+
+
+    public static int sum(int n ) {
+        if (n == 1) return 1;
+        return n * n + sum(n-1);
+    }
+
+    public static int sum_arr(int[] arr, int n) {
+        if (n == 0) {
+            return 0;
+        }
+        return arr[n - 1] + sum_arr(arr , n -1);
+    }
+
+    public static double power(int b , int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return pow(b, n) + power(b , n-1);
+    }
+
+    public static int revers(int n, Scanner sc) {
+        if (n == 0) {
+            return 0;
+        }
+
+        int stack = sc.nextInt();
+        revers(n - 1, sc);
+        System.out.print(stack + " ");
+
+        return 1;
+    }
 }
+
